b0811dca786dbf689d22e72d58055fa4257056ff
Leonardorossato <leonardorossato21@gmail.com>
2023-02-13 20:51:26 -0300

    update user.schema.ts

--- src/users/schema/user.schema.ts
+++ src/users/schema/user.schema.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { now } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true, type: String })
+  name: string;
+
+  @Prop({ required: true, type: String })
+  username: string;
+
+  @Prop({ required: true, type: String })
+  password: string;
+
+  @Prop({ required: true, type: String, default: 'User' })
+  profile: string;
+
+  @Prop({ type: now() })
+  createdAt?: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
